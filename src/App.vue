b3347cdd17f3c724e0f66b14fb532e3aa7f0aa39
Sujin <sjnnus@gmail.com>
2024-02-09 11:47:07 +0900

    240209 bootstrap ex

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- nav bar -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">NPIUS</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Notice</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <!-- main -->
+  <div class="container mt-4">
+    <h4> North Pacific International Undersea Station </h4>
+    <p> 모든 임직원의 안전한 북태평양해저기지 생활을 응원합니다. </p>
+  </div>
+<button type="button" class="btn btn-secondary">NPIUS 👉</button>
+
+<!-- board list -->
+<BoardList :contents="contents" />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// components import
+import boardContents from './assets/contents.js';
+import BoardList from './components/BoardList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    BoardList: BoardList,
+  },
+  data() {
+    return {
+      contents : boardContents,
+    }
+  },
 }
 </script>
 
@@ -21,6 +64,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/BoardList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="board">
+    <div v-for="(detail, number) in contents" :key="number">
+      <h5> {{ detail.title }} </h5>
+        <p> {{ detail.date }} </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BoardList',
+    props: {
+      contents: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
